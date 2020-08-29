3bf06c01daacca59256fee23f1d72490ded68e99
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2020-08-29 19:03:34 +0530

    Added a couple of parser tests

--- src/tests/Parser.test.ts
+++ src/tests/Parser.test.ts
@@ -9,8 +9,8 @@ test("single line input with one request", async () => {
 	], 0, context);
 
 	expect(request).toBeDefined();
-	expect(request?.method).toBe("GET");
-	expect(request?.url).toBe("http://httpbin.org");
+	expect(request!.method).toBe("GET");
+	expect(request!.url).toBe("http://httpbin.org");
 });
 
 test("get request on first and only non-blank line", async () => {
@@ -22,8 +22,8 @@ test("get request on first and only non-blank line", async () => {
 	], 0, context);
 
 	expect(request).toBeDefined();
-	expect(request?.method).toBe("GET");
-	expect(request?.url).toBe("http://httpbin.org");
+	expect(request!.method).toBe("GET");
+	expect(request!.url).toBe("http://httpbin.org");
 });
 
 test("get request with blank lines around", async () => {
@@ -36,8 +36,8 @@ test("get request with blank lines around", async () => {
 	], 1, context);
 
 	expect(request).toBeDefined();
-	expect(request?.method).toBe("GET");
-	expect(request?.url).toBe("http://httpbin.org");
+	expect(request!.method).toBe("GET");
+	expect(request!.url).toBe("http://httpbin.org");
 });
 
 test("post request with one line body", async () => {
@@ -50,9 +50,9 @@ test("post request with one line body", async () => {
 	], 0, context);
 
 	expect(request).toBeDefined();
-	expect(request?.method).toBe("POST");
-	expect(request?.url).toBe("http://httpbin.org");
-	expect(request?.body).toBe("body goes here");
+	expect(request!.method).toBe("POST");
+	expect(request!.url).toBe("http://httpbin.org");
+	expect(request!.body).toBe("body goes here");
 });
 
 test("get request with an ending", async () => {
@@ -69,7 +69,80 @@ test("get request with an ending", async () => {
 	], 0, context);
 
 	expect(request).toBeDefined();
-	expect(request?.method).toBe("POST");
-	expect(request?.url).toBe("http://httpbin.org");
-	expect(request?.body).toBe("body goes here");
+	expect(request!.method).toBe("POST");
+	expect(request!.url).toBe("http://httpbin.org");
+	expect(request!.body).toBe("body goes here");
+});
+
+test("pokemon graphql", async () => {
+	const context = makeMockContext();
+
+	const queryText = "query {\n" +
+		"  pokemon(name: \"Pikachu\") {\n" +
+		"    id\n" +
+		"    number\n" +
+		"    name\n" +
+		"    attacks {\n" +
+		"      special {\n" +
+		"        name\n" +
+		"        type\n" +
+		"        damage\n" +
+		"      }\n" +
+		"      fast {\n" +
+		"        name\n" +
+		"        type\n" +
+		"        damage\n" +
+		"      }\n" +
+		"    }\n" +
+		"    evolutions {\n" +
+		"      id\n" +
+		"      number\n" +
+		"      name\n" +
+		"      attacks {\n" +
+		"        fast {\n" +
+		"          name\n" +
+		"          type\n" +
+		"          damage\n" +
+		"        }\n" +
+		"      }\n" +
+		"    }\n" +
+		"  }\n" +
+		"}";
+
+	const request = await extractRequest([
+		"POST https://graphql-pokemon.now.sh/",
+		"Content-Type: application/graphql",
+		"",
+		queryText,
+	], 0, context);
+
+	expect(request).toBeDefined();
+	expect(request!.method).toBe("POST");
+	expect(request!.url).toBe("https://graphql-pokemon.now.sh/");
+	expect(Array.from(request!.headers)).toEqual([["content-type", "application/graphql"]]);
+	expect(request!.body).toBe(queryText);
+});
+
+test("slack post message", async () => {
+	const context = makeMockContext();
+
+	const request = await extractRequest([
+		"POST https://slack.com/api/chat.postMessage",
+		"Content-Type: application/json; charset=UTF-8",
+		"Authorization: Bearer token",
+		"",
+		JSON.stringify({
+			channel: "#general",
+			text: "A message from the API!",
+		}),
+	], 0, context);
+
+	expect(request).toBeDefined();
+	expect(request!.method).toBe("POST");
+	expect(request!.url).toBe("https://slack.com/api/chat.postMessage");
+	expect(Array.from(request!.headers)).toEqual([
+		["authorization", "Bearer token"],
+		["content-type", "application/json; charset=UTF-8"],
+	]);
+	expect(request!.body).toBe("{\"channel\":\"#general\",\"text\":\"A message from the API!\"}");
 });
