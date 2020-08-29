03cbe699c2c43842784e209b39d316c06a9461ed
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2020-08-29 18:41:18 +0530

    Upgrade dependencies and fix tests

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,8 +1,12 @@
-export function makeMockContext() {
+import { Context } from "../scripts/Context";
+
+export function makeMockContext(): Context {
 	return {
 		data: {},
 		run: jest.fn(),
 		on: jest.fn(),
 		off: jest.fn(),
+		emit: jest.fn(),
+		authHeader: jest.fn(),
 	};
 }
