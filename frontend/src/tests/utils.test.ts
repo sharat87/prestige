b48ff393edd079d1c18166cc027a8ab8e8c82ff7
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2021-02-07 20:04:22 +0530

    Use typescript linting plugins

--- frontend/src/tests/utils.test.ts
+++ frontend/src/tests/utils.test.ts
@@ -1,7 +1,7 @@
 import { isPromise } from "../scripts/utils"
 
 test("promise object is okayed as a promise", async () => {
-	expect(isPromise(new Promise((resolve) => resolve()))).toBeTruthy()
+	expect(isPromise(new Promise((resolve) => resolve(void 0)))).toBeTruthy()
 	expect(isPromise(Promise.resolve())).toBeTruthy()
 	// In the below assertion, we catch the rejection to avoid Node's uncaught rejections warning.
 	expect(isPromise(Promise.reject().catch(() => void 0))).toBeTruthy()
