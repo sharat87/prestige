ca9db4ba25439ccbba03484ee5821ca783bfa47c
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2021-03-20 22:12:23 +0530

    Added linting and the base for theming

---
+++ frontend/src/scripts/ColorPaletteModal.ts
@@ -0,0 +1,48 @@
+import m from "mithril"
+import Modal from "./Modal"
+
+export default { view }
+
+function view(): m.Children {
+	return m(
+		Modal,
+		{
+			title: "Color Palette",
+		},
+		[
+			m(CakeRow, { prefix: "gray" }),
+			m(CakeRow, { prefix: "primary" }),
+			m(CakeRow, { prefix: "blue" }),
+			m(CakeRow, { prefix: "green" }),
+			m(CakeRow, { prefix: "yellow" }),
+			m(CakeRow, { prefix: "red" }),
+		],
+	)
+}
+
+function isVarAvailable(name: string): boolean {
+	return getComputedStyle(document.documentElement).getPropertyValue("--" + name) !== ""
+}
+
+const CakeRow = {
+	view(vnode: m.VnodeDOM<{ prefix: string }>) {
+		const { prefix } = vnode.attrs
+		return m("p.tc", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(n => m(Cake, { name: `${prefix}-${n}` })))
+	},
+}
+
+const Cake = {
+	view(vnode: m.VnodeDOM<{ name: string }>) {
+		const { name } = vnode.attrs
+		return m(
+			"span.dib.w2.h2.ma1.br2",
+			{
+				title: name,
+				style: {
+					boxShadow: isVarAvailable(name) ? "inset 0 0 2px #0009" : "none",
+					backgroundColor: "var(--" + name + ")",
+				},
+			},
+		)
+	},
+}
